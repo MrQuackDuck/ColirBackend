cc87f5c29f0e95fd786ffe23da543e5c803b2b2f
mrquackduck <mrquackduckk@gmail.com>
2024-07-25 15:21:27 +0300

    Update

--- Colir.WebApi/Interfaces/Controllers/IRoomController.cs
+++ Colir.WebApi/Interfaces/Controllers/IRoomController.cs
@@ -9,7 +9,7 @@ public interface IRoomController
     Task<ActionResult<RoomModel>> GetRoomInfo(GetRoomInfoModel model);
     Task<ActionResult<RoomModel>> CreateRoom(CreateRoomModel model);
     Task<ActionResult<RoomModel>> JoinRoom(JoinRoomModel model);
-    Task<ActionResult<RoomModel>> LeaveRoom(LeaveRoomModel model);
+    Task<ActionResult> LeaveRoom(LeaveRoomModel model);
     Task<ActionResult<DateTime>> GetLastTimeReadChatModel(GetLastTimeReadChatModel model);
     Task<ActionResult> UpdateLastTimeReadChat(UpdateLastTimeReadChatModel model);
     Task<ActionResult> KickMember(KickMemberModel model);
