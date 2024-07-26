d70e2885eba11e7b4ec890c4445aff62e107b78e
mrquackduck <mrquackduckk@gmail.com>
2024-07-26 22:20:10 +0300

    Update

--- Colir.WebApi/Program.cs
+++ Colir.WebApi/Program.cs
@@ -1,10 +1,42 @@
+using System.IO.Abstractions;
 using System.Text;
+using Colir.BLL;
+using Colir.BLL.Factories;
+using Colir.BLL.Interfaces;
+using Colir.BLL.Services;
 using DAL;
+using DAL.Interfaces;
+using DAL.Repositories;
+using DAL.Repositories.Related;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.SwaggerGen.ConventionalRouting;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Adding DAL services
+builder.Services.AddScoped<IAttachmentRepository, AttachmentRepository>();
+builder.Services.AddScoped<ILastTimeUserReadChatRepository, LastTimeUserReadChatRepository>();
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddScoped<IReactionRepository, ReactionRepository>();
+builder.Services.AddScoped<IFileSystem, FileSystem>();
+builder.Services.AddScoped<IRoomFileManager, RoomFileManager>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserSettingsRepository, UserSettingsRepository>();
+builder.Services.AddScoped<IUserStatisticsRepository, UserStatisticsRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Adding BLL services
+builder.Services.AddAutoMapper(typeof(AutomapperProfile));
+builder.Services.AddScoped<IAttachmentRepository, AttachmentRepository>();
+builder.Services.AddScoped<IHexColorGenerator, HexColorGenerator>();
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddScoped<IRoomCleanerFactory, RoomCleanerFactory>();
+builder.Services.AddScoped<IRoomService, RoomService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserStatisticsService, UserStatisticsService>();
+
 builder.Services.AddDbContext<ColirDbContext>();
 builder.Services.AddControllers();
 
@@ -35,9 +67,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 // Adding CORS policy
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
+    options.AddDefaultPolicy(defaultPolicy =>
     {
-        builder.AllowAnyMethod()
+        defaultPolicy.AllowAnyMethod()
             .AllowAnyHeader()
             .SetIsOriginAllowed(origin => true)
             .AllowCredentials();
