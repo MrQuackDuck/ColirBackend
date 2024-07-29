7d0ac9e0b187d3ddf9dcc3cc46f1e7321055faa2
mrquackduck <mrquackduckk@gmail.com>
2024-07-30 01:02:21 +0300

    Update

--- Colir.WebApi/Communication/RequestModels/Room/CreateRoomModel.cs
+++ Colir.WebApi/Communication/RequestModels/Room/CreateRoomModel.cs
@@ -3,6 +3,5 @@
 public class CreateRoomModel
 {
     public string Name { get; set; } = default!;
-    public int OwnerHexId { get; set; } = default!;
     public DateTime? ExpiryDate { get; set; } = null;
 }
