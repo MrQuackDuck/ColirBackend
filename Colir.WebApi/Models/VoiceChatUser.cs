dfcba3722450ea6f2394f83e312d1e4b85f921e2
mrquackduck <mrquackduckk@gmail.com>
2024-07-31 01:17:03 +0300

    Update

--- Colir.WebApi/Models/VoiceChatUser.cs
+++ Colir.WebApi/Models/VoiceChatUser.cs
@@ -5,4 +5,6 @@ public class VoiceChatUser
     public int Id { get; set; }
     public bool IsMuted { get; set; }
     public bool IsDefeaned { get; set; }
+    public bool IsVideoEnabled { get; set; }
+    public bool IsStreamEnabled { get; set; }
 }
