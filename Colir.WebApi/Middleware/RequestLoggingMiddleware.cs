f05cac2ad6a891f780256a6537a7089cb17bba86
MrQuackDuck <mrquackduckk@gmail.com>
2024-11-19 09:25:43 +0200

    Update

--- Colir.WebApi/Middleware/RequestLoggingMiddleware.cs
+++ Colir.WebApi/Middleware/RequestLoggingMiddleware.cs
@@ -9,10 +9,22 @@ public class RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggi
     public async Task InvokeAsync(HttpContext context)
     {
         var stopwatch = Stopwatch.StartNew();
-        // Clone the request body so we can read it multiple times
-        context.Request.EnableBuffering();
-        var requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
-        context.Request.Body.Position = 0;
+
+        string requestBody;
+        double requestSizeKb = Math.Round((context.Request.ContentLength ?? 0) / 1024.0, 2);
+
+        if (context.Request.ContentType == "application/json")
+        {
+            // Clone the request body so we can read it multiple times
+            context.Request.EnableBuffering();
+            requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
+            context.Request.Body.Position = 0;
+        }
+        else
+        {
+            // If the content type is not JSON, just log the content type
+            requestBody = $"<{context.Request.ContentType}>";
+        }
 
         // Capture the response using a memory stream
         var originalBodyStream = context.Response.Body;
@@ -38,12 +50,14 @@ public class RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggi
             logger.LogInformation(
                 "Request: [{Method}] {Url} - Status: {StatusCode} - Elapsed time: {Duration}ms\n" +
                 "Request Body: {RequestBody}\n" +
+                "Request Size: {RequestSize} Kb\n" +
                 "Response Size: {ResponseSize} Kb",
                 context.Request.Method,
                 context.Request.Path,
                 context.Response.StatusCode,
                 stopwatch.ElapsedMilliseconds,
                 requestBody.Length == 0 ? "<empty>" : requestBody,
+                requestSizeKb,
                 responseSizeKb);
         }
     }
