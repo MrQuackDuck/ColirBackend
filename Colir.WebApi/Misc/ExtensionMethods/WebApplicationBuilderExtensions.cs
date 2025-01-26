a35c8676c00bf95ccf9991b71e83edb7a6454f4b
MrQuackDuck <mrquackduckk@gmail.com>
2025-01-27 00:36:27 +0200

    Update (Enabled database to run by default with Docker Compose)

--- Colir.WebApi/Misc/ExtensionMethods/WebApplicationBuilderExtensions.cs
+++ Colir.WebApi/Misc/ExtensionMethods/WebApplicationBuilderExtensions.cs
@@ -8,7 +8,16 @@ public static class WebApplicationBuilderExtensions
     public static void AddPostgreDbContext(this WebApplicationBuilder builder)
     {
         var host = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!.Replace("{host}", host);
+        var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "Colir";
+        var username = Environment.GetEnvironmentVariable("DB_USERNAME") ?? "USERNAME";
+        var password = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "USER_PASSWORD";
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!
+            .Replace("{Host}", host)
+            .Replace("{DbName}", dbName)
+            .Replace("{DbUsername}", username)
+            .Replace("{DbPassword}", password);
+
         builder.Services.AddDbContext<ColirDbContext>(options =>
             options.UseNpgsql(connectionString));
     }
